6564c7484289d6e9f68b23567d09a23e1a79fb28
Casey Vega <cvega@users.noreply.github.com>
2019-04-03 16:21:41 -0700

    Update Greetings.groovy

--- src/vega/demo/Greetings.groovy
+++ src/vega/demo/Greetings.groovy
@@ -2,19 +2,36 @@ package vega.demo
 
 
 class Greetings implements Serializable {
-    def cmd, lang, name
-
+    private def cmd, lang, name
 
     Greetings(cmd, String lang, String name) {
         this.cmd  = cmd
         this.lang = lang
         this.name = name ?: 'Jenkins User'
     }
+
+
+    def sayHello() {
+        say(hello(this.lang))
+    }
+    
+
+    def sayGoodbye() {
+        say(goodbye(this.lang))
+    }  
+
+    
+    def private say(String greeting) {
+        this.cmd.echo "${greeting} ${this.name}"
+    }
     
     
     def private hello(String lang) {
         switch(lang) {
+            case "chinese": return "你好"; break;
             case "english": return "Hello"; break;
+            case "hindi": return "नमस्ते"; break;
+            case "japanese": return "こんにちは"; break;
             case "spanish": return "Hola"; break;
             default: return "Welcome"
         }
@@ -23,21 +40,12 @@ class Greetings implements Serializable {
     
     def private goodbye(String lang) {
         switch(lang) {
+            case "chinese": return "再见"; break;
             case "english": return "Good bye"; break;
+            case "hindi": return "अलविदा"; break;
+            case "japanese": return "さようなら"; break;
             case "spanish": return "Adiós"; break;
             default: return "Godspeed"
         }
     }
-    
-    
-    def sayHello() {
-        def greeting = hello(this.lang)
-        this.cmd.echo "${greeting} ${this.name}"
-    }
-    
-    
-    def sayGoodbye() {
-        def greeting = goodbye(this.lang)
-        this.cmd.echo "${greeting} ${this.name}"
-    }  
 }
