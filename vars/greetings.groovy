b029b0ffb3bc86d6f91f02dcf0b19e9332a529df
Casey Vega <cvega@users.noreply.github.com>
2019-04-03 15:10:06 -0700

    Update greetings.groovy

--- vars/greetings.groovy
+++ vars/greetings.groovy
@@ -2,7 +2,7 @@ import vega.demo.Greetings
 
 
 def call(String lang, String name) {
-  def greetings = new Greetings(lang, name);
+  def greetings = new Greetings(this, lang, name);
   
   greetings.sayHello
   greetings.sayGoodbye
