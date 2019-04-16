a2d225f856111e806547efbffcbac58d7fbd562b
Casey Vega <cvega@users.noreply.github.com>
2019-04-16 13:02:34 -0700

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -1,1 +1,16 @@
+import vega.demo.Filter
 
+
+def call(String jenkinsfile, String type) {
+    def filter = new Filter(this, jenkinsfile);
+  
+  if (type == "security") {
+    filter.security()
+  }
+  elif (type == "profanity") {
+    filter.profanity()
+  }
+  else {
+    echo "*** INVALID FILTER NAME ***"
+  }
+}
