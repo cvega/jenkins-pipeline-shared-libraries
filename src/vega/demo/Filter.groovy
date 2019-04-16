a5e97c43fd874dbf68ab84fc68d692cc6798f7aa
Casey Vega <casey.vega@gmail.com>
2019-04-16 12:56:41 -0700

    add filter

--- src/vega/demo/Filter.groovy
+++ src/vega/demo/Filter.groovy
@@ -1,1 +1,44 @@
+package vega.demo
+
+
+class Filter implements Serializable {
+    private def cmd, jenkinsfile
+
+    Greeting(Object cmd, String jenkinsfile) {
+        this.cmd  = cmd
+        this.jenkinsfile = jenkinsfile
+    }
+
+
+    def public security-regex() {
+        security()
+    }
+
+
+    def public profanity-regex() {
+        profanity()
+    }
+
+
+    def private scrub(String alert) {
+        this.cmd.echo "${alert} alert!"
+        this.cmd.currentBuild.result = 'FAILURE'
+        return
+    }
+
+
+    def private security() {
+        if ( this.jenkinsfile ==~ /(?s).*credentials.*/) {
+            scrub("secruity")
+            return
+        }
+    }
+
+    def private profanity(String jenkinsfile) {
+        if ( this.jenkinsfile ==~ /(?s).*fightclub.*/) {
+            scrub("profanity")
+            return
+        }
+    }
+}
 
