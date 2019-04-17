affa93c2bca1b6195ddbeed5b85dbdb76271b14f
Casey Vega <cvega@users.noreply.github.com>
2019-04-17 11:49:57 -0700

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -3,17 +3,12 @@ import vega.demo.Filter
 
 def call(String jenkinsfile) {
   def filter = new Filter(this, jenkinsfile)
-  def screen = [filter.security(), filter.profanity()]
   
-  screen.each {
-    println "here i am"
-    println "$it"
-    if (!it) {
-      println "here to"
-      currentBuild.result = 'ABORT'
-      error()
+  [filter.security(), filter.profanity()].each {
+    if (it) {
+      currentBuild.result = 'ABORTED'
+      error("*** build aborted by filter library ***")
+      return
     }
   }
-  
-  return
 }
