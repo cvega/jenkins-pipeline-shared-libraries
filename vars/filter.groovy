3d01d7a00f0c24ebf660c6f5c4d9d48dba6f379d
Casey Vega <cvega@users.noreply.github.com>
2019-04-17 09:37:06 -0700

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -5,13 +5,13 @@ def call(String jenkinsfile, String type) {
   def filter = new Filter(this, jenkinsfile);
   
   if (type == "security") {
-    if (filter.securityRegexp() == true) {
-      currentBuild.result = 'ABORT'
+    if (filter.securityRegexp() == 'FAILURE') {
+      currentBuild.result = "${filter.securityRegexp()}"
     }
   }
   else if (type == "profanity") {
-    if (filter.profanityRegexp() == true) {
-      currentBuild.result = 'ABORT'
+    if (filter.profanityRegexp() == 'FAILURE') {
+      currentBuild.result = "${filter.profanityRegexp()}"
     }
   }
   else {
