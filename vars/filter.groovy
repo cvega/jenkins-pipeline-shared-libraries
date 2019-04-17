7d06af246bcbba3ab0cbcb65157910b4aa5e4f16
Casey Vega <cvega@users.noreply.github.com>
2019-04-17 11:20:29 -0700

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -1,19 +1,18 @@
 import vega.demo.Filter
 
 
+def clean(boolean filter) {
+  if (!filter) {
+    currentBuild.result = 'ABORT'
+    error()
+    return
+  }
+}
+
+
 def call(String jenkinsfile) {
   def filter = new Filter(this, jenkinsfile);
   
-  if (filter.securityRegexp() == 'FAILURE') {
-      currentBuild.result = "FAILURE"
-      error('Stopping early…')
-  }
-  else if (filter.profanityRegexp == 'FAILURE') {
-      currentBuild.result = "${filter.profanityRegexp()}"
-      error('Stopping early…')
-  }
-  else {
-    echo "*** INVALID FILTER NAME ***"
-  }
-  return
+  this.clean(filter.security())
+  this.clean(filter.profanity())
 }
