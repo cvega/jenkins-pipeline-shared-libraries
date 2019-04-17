8215546a0d1498bc4fdede58b2dd549ad3526029
Casey Vega <cvega@users.noreply.github.com>
2019-04-17 10:57:36 -0700

    Update filter.groovy

--- vars/filter.groovy
+++ vars/filter.groovy
@@ -4,14 +4,11 @@ import vega.demo.Filter
 def call(String jenkinsfile) {
   def filter = new Filter(this, jenkinsfile);
   
-  if (type == "security") {
-    if (filter.securityRegexp() == 'FAILURE') {
+  if (filter.securityRegexp() == 'FAILURE') {
       currentBuild.result = "${filter.securityRegexp()}"
       error('Stopping early…')
     }
-  }
-  else if (type == "profanity") {
-    if (filter.profanityRegexp() == 'FAILURE') {
+  else if (filter.profanityRegexp() == 'FAILURE') {
       currentBuild.result = "${filter.profanityRegexp()}"
       error('Stopping early…')
     }
